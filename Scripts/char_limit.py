4a181d6a2b92542326b0588d54f8578d01af0aae
Denis Naumov <94964844+denisnaumov0506@users.noreply.github.com>
2023-06-14 09:23:58 +0200

    Add files via upload

---
+++ Scripts/char_limit.py
@@ -0,0 +1,19 @@
+import csv
+
+apps = ['netflix', 'youtube', 'whatsapp', 'paypal', 'amazon_shop']
+
+for app in apps:
+
+    list_ = []
+
+    with open(f'fdist_{app}.csv', 'r', newline='\n') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='|')
+        for row in reader:
+            list_.append(row)
+
+    # sort the list
+    list_.sort(key=lambda x: int(x[0]))
+
+    # now calculate the percentage of at and below 280
+
+    print(f'Result for {app}: ', sum(int(x[1]) for x in list_ if int(x[0]) <= 280)/sum(int(x[1]) for x in list_))
