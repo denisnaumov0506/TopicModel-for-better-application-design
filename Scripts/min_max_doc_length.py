21f94c4bd2b6502b02dae9816eeee9b752791941
Denis Naumov <94964844+denisnaumov0506@users.noreply.github.com>
2023-06-30 17:01:44 +0200

    Update min_max_doc_length.py
    cleaned comments

--- Scripts/min_max_doc_length.py
+++ Scripts/min_max_doc_length.py
@@ -13,11 +13,12 @@ for app in apps:
 
     data = []
 
+    # read the frequency distribution
     with open(f'fdist_{app}.csv', 'r', newline='\n') as f:
         reader = csv.reader(f, delimiter=',', quotechar='|')
         for row in reader:
             data.append(row)
 
-    # calculate the average
+    # calculate and print the min and max doc length
     print(f'The max doc length of {app} is: ', max(int(x[0]) for x in data))
     print(f'The min doc length of {app} is: ', min(int(x[0]) for x in data), '\n\n')
